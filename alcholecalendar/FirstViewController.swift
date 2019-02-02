d554c2c576383f27566d4a53f26b8da338c157f7
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-02-02 17:24:01 +0900

    Initial Commit

---
+++ alcholecalendar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  alcholecalendar
+//
+//  Created by Kan Nakamura on 2019/02/02.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ alcholecalendar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  alcholecalendar
+//
+//  Created by Kan Nakamura on 2019/02/02.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
